7e057e59995a643d509602fd3b9e94bec8d534d5
AVSheard <a_sheard@yahoo.co.uk>
2020-04-02 12:51:50 +0100

    got the grid to resemble the shape of a noughts and crosses grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="grid-container">
+			<div className="grid-item">1</div>
+			<div className="grid-item">2</div>
+			<div className="grid-item">3</div>
+			<div className="grid-item">4</div>
+			<div className="grid-item">5</div>
+			<div className="grid-item">6</div>
+			<div className="grid-item">7</div>
+			<div className="grid-item">8</div>
+			<div className="grid-item">9</div>
+		</div>
+	);
 }
 
 export default App;

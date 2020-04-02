90edfb183af3d06609c86d2a61ed9c4f728572f7
AVSheard <a_sheard@yahoo.co.uk>
2020-04-02 16:28:26 +0100

    a second click no longer overwrites first value and logic is in place that tests if there is a winner

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -4,24 +4,52 @@ import React, { Component } from "react";
 
 const cross = "x";
 const nought = "o";
+const winConditions = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+];
 
 export default class Grid extends Component {
 	state = {
-		turn: true,
+		noughtTurn: true,
+		inplay: true,
 		values: ["", "", "", "", "", "", "", "", ""]
 	};
 
 	handleClick = (index) => {
-		this.setState((currentState) => {
-			const arr = [...currentState.values];
-			if (this.state.turn) {
-				arr[index] = nought;
-			} else {
-				arr[index] = cross;
-			}
-			return { values: arr, turn: !currentState.turn };
-		});
+		if (this.state.values[index] === "") {
+			this.setState((currentState) => {
+				const arr = [...currentState.values];
+				if (this.state.noughtTurn) {
+					arr[index] = nought;
+				} else {
+					arr[index] = cross;
+				}
+				return { values: arr, noughtTurn: !currentState.noughtTurn };
+			});
+		}
 	};
+
+	componentDidUpdate(prevProps, prevState) {
+		const { values } = this.state;
+		if (prevState !== values) {
+			winConditions.forEach((condition) => {
+				if (
+					values[condition[0]] === values[condition[1]] &&
+					values[condition[0]] === values[condition[2]] &&
+					values[condition[0]] !== ""
+				) {
+					console.log("win");
+				}
+			});
+		}
+	}
 
 	render() {
 		return (

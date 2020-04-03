ac094499766d46e1e1f77e5b780442faef29636d
AVSheard <a_sheard@yahoo.co.uk>
2020-04-03 15:18:29 +0100

    made a statistics commponent to display how many times each player has won

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import "./App.css";
 import Grid from "./components/Grid";
 import Header from "./components/Header";
 import Reset from "./components/Reset";
+import Statistics from "./components/Statistics";
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<Statistics />
 			<Reset />
 			<Grid />
 		</div>
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React, { Component } from "react";
+
+export default class Statistics extends Component {
+	render() {
+		return (
+			<h2 className="stats">
+				Player 1 victories: 0 Draws: 0 Player 2 victories: 0
+			</h2>
+		);
+	}
+}

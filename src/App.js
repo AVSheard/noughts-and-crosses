a1c127933cc3d16b879f67ddc322f0b994a67807
AVSheard <a_sheard@yahoo.co.uk>
2020-10-20 11:35:29 +0100

    Added AIToggle component to App

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import Grid from "./components/Grid";
 import Header from "./components/Header";
 import Reset from "./components/Reset";
+import AIToggle from "./components/AIToggle";
 
 function App() {
 	return (
 		<div>
 			<Header />
+			<AIToggle />
 			<Reset />
 			<Grid />
 		</div>

2a7f593dd5d433ae1d71361f13c8ae0e0ec4a2d0
AVSheard <a_sheard@yahoo.co.uk>
2020-10-21 16:05:39 +0100

    Added DifficultyToggle component to App

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Grid from "./components/Grid";
 import Header from "./components/Header";
 import Reset from "./components/Reset";
 import AIToggle from "./components/AIToggle";
+import DifficultyToggle from "./components/DifficultyToggle";
 
 function App() {
 	return (
 		<div>
 			<Header />
 			<AIToggle />
+			<DifficultyToggle />
 			<Reset />
 			<Grid />
 		</div>

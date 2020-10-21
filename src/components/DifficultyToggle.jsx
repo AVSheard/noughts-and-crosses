f36ad942ef7b64d3a15ac6010a87199b1027fecb
AVSheard <a_sheard@yahoo.co.uk>
2020-10-21 19:02:03 +0100

    made layout difficulty toggle

--- src/components/DifficultyToggle.jsx
+++ src/components/DifficultyToggle.jsx
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 
 export default class DifficultyToggle extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<form>
+				<p>Choose difficulty:</p>
+				<label>Easy</label>
+				<input type="radio" id="easy" name="difficulty" value="easy"></input>
+				<label>Normal</label>
+				<input
+					type="radio"
+					id="normal"
+					name="difficulty"
+					value="normal"></input>
+			</form>
+		);
 	}
 }
